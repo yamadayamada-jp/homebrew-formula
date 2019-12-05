ec9fd293d89bc8c29b5f46c3f15d3e16d42d3ce5
Yamada Koki <yamada.koki@hamee.co.jp>
2019-12-05 17:39:07 +0900

    formula更新

--- formula/homebrew-formula.rb
+++ formula/homebrew-formula.rb
@@ -1,14 +1,10 @@
 class HomebrewFormula < Formula
   desc ""
   homepage ""
-  url "https://github.com/yamadayamada-jp/homebrew-formula/releases/download/0.0.2/helloworld"
+  url "https://github.com/yamadayamada-jp/homebrew-hello-world/releases/download/0.0.2/helloworld"
   sha256 "88fd602a930bc7c0bb78c385f3cb70e976a0cdc3517020be32f19aae8c8eba17"
 
   def install
     bin.install "helloworld"
-  end
-
-  test do
-    system "false"
   end
 end
