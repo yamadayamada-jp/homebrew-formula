dd11574561818c9947321a9bd274565f103882b7
yamadayamada-jp <i1024076@gmail.com>
2019-12-05 17:08:20 +0900

    formula更新

--- formula/homebrew-formula.rb
+++ formula/homebrew-formula.rb
@@ -1,7 +1,7 @@
 class HomebrewFormula < Formula
   desc ""
   homepage ""
-  url "https://github.com/yamadayamada-jp/homebrew-formula/releases/download/0.0.1/helloworld.sh"
+  url "https://github.com/yamadayamada-jp/homebrew-formula/releases/download/0.0.2/helloworld"
   sha256 "88fd602a930bc7c0bb78c385f3cb70e976a0cdc3517020be32f19aae8c8eba17"
 
   def install
